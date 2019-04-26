d6307dad92576fed5d8afe8ca3699be0ce64e445
Brian Wilcox <brianwilcox@stanford.edu>
2019-04-26 17:31:09 -0400

    Initial OpenCV Commit

---
+++ hello_opencv.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv){
+	if (argc != 2){
+		printf("usage: hello_opencv.out <Image_Path>\n");
+		return -1;
+	}
+	cv::Mat image;
+	image = cv::imread(argv[1], 1);
+	// check for null pointer in data implying invalid file path	
+	if (!image.data){
+		printf("No image data \n");
+		return -1;
+	}
+
+	cv::namedWindow("Test Image", WINDOW_AUTOSIZE);
+	cv::imshow("Test Image", image);
+
+	cv::waitKey(0);
+
+	return 0; 	
+}	
