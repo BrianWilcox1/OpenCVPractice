f7ffa088fbda255a21040d6ee458969c1fe8b0d0
Brian Wilcox <brianwilcox@stanford.edu>
2019-04-27 12:04:49 -0400

    Added support for built-in advanced morphology operations

--- include/morphology.h
+++ include/morphology.h
@@ -5,4 +5,6 @@ class Morphology{
     public:
         cv::Mat dilation(const cv::Mat& image, const int dilation_type, const int dilation_size);
 		cv::Mat erosion(const cv::Mat& image, const int erosion_type, const int erosion_size);
+        cv::Mat morphologyAdvanced(const cv::Mat& image, const int morph_elem, 
+                                   const int operation, const int size);
 };
--- src/morphology.cpp
+++ src/morphology.cpp
@@ -1,7 +1,8 @@
 #include <morphology.h>
 
 cv::Mat Morphology::dilation(const cv::Mat& image, const int dilation_type, const int dilation_size){
-    cv::Mat struct_el = cv::getStructuringElement(dilation_type, cv::Size(2*dilation_size + 1, 2*dilation_size + 1),
+    cv::Mat struct_el = cv::getStructuringElement(dilation_type, 
+                                           cv::Size(2*dilation_size + 1, 2*dilation_size + 1),
                                             cv::Point(dilation_size, dilation_size));
     cv::Mat dilation_out;
     cv::dilate(image, dilation_out, struct_el);
@@ -13,4 +14,11 @@ cv::Mat Morphology::erosion(const cv::Mat& image, const int erosion_type, const 
     cv::Mat erosion_out;
     cv::erode(image, erosion_out, struct_el);
     return erosion_out;   
+}
+
+cv::Mat morphologyAdvanced(const cv::Mat& image, const int morph_elem, const int operation, const int size){
+    cv::Mat struct_el = cv::getStructuringElement(morph_elem, cv::Size(2*size + 1, 2*size + 1), cv::Point(size, size));    
+    cv::Mat morph_out;
+    cv::morphologyEx(image, morph_out, operation, struct_el);
+    return morph_out;
 }
