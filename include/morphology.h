740d9dd2e1049213a2cc1dc8f6f3a2ce7d35269c
Brian Wilcox <brianwilcox@stanford.edu>
2019-04-27 01:23:37 -0400

    Added Some Morphology operations and close example

---
+++ include/morphology.h
@@ -0,0 +1,8 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+
+class Morphology{
+    public:
+        cv::Mat dilation(const cv::Mat& image, const int dilation_type, const int dilation_size);
+		cv::Mat erosion(const cv::Mat& image, const int erosion_type, const int erosion_size);
+};
