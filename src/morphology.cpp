545f28551b0d9a672299c746fef128e8d3ba99ae
Brian Wilcox <brianwilcox@stanford.edu>
2019-04-27 12:13:04 -0400

    Cleaned up function definitions for dilate and erode.

--- src/morphology.cpp
+++ src/morphology.cpp
@@ -1,16 +1,13 @@
 #include <morphology.h>
 
-cv::Mat Morphology::dilation(const cv::Mat& image, const int dilation_type, const int dilation_size){
-    cv::Mat struct_el = cv::getStructuringElement(dilation_type, 
-                                           cv::Size(2*dilation_size + 1, 2*dilation_size + 1),
-                                            cv::Point(dilation_size, dilation_size));
+cv::Mat Morphology::dilation(const cv::Mat& image, const int type, const int size){
+    cv::Mat struct_el = cv::getStructuringElement(type, cv::Size(2*size + 1, 2*size + 1), cv::Point(size, size));
     cv::Mat dilation_out;
     cv::dilate(image, dilation_out, struct_el);
     return dilation_out;   
 }
-cv::Mat Morphology::erosion(const cv::Mat& image, const int erosion_type, const int erosion_size){
-    cv::Mat struct_el = cv::getStructuringElement(erosion_type, cv::Size(2*erosion_size + 1, 2*erosion_size + 1),
-                                            cv::Point(erosion_size, erosion_size));    
+cv::Mat Morphology::erosion(const cv::Mat& image, const int type, const int size){
+    cv::Mat struct_el = cv::getStructuringElement(type, cv::Size(2*size + 1, 2*size + 1), cv::Point(size, size));
     cv::Mat erosion_out;
     cv::erode(image, erosion_out, struct_el);
     return erosion_out;   
