32784389615b57f5efc840bbe86ffefbe37b5c6c
Brian Wilcox <brianwilcox@stanford.edu>
2019-04-27 14:50:23 -0400

    Added some basic filtering examples/libraries

---
+++ include/filtering.h
@@ -0,0 +1,8 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+class Filters{
+    public:
+        cv::Mat sharpen(const cv::Mat& image);
+        cv::Mat conv2D(const cv::Mat& image,  const cv::Mat& kernel);
+};
